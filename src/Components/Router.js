241a07712af78d956758ff92fc965df1fbe60272
jinwook-song <wlsdnr129@naver.com>
2021-04-01 18:08:41 +0900

    lecture 3.2 CSS part 3

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -7,10 +7,12 @@ import {
 } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
+import Header from "Components/Header";
 import Search from "Routes/Search";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
